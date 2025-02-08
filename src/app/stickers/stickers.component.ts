6aacc07100c31b09b1344802ec1a6170249147e8
Tung Nguyen <tung.nguyen@codelink.io>
2025-02-08 16:28:43 +0700

    Version 1.3.0: Sticker search params is now in URL
    Put sticker search params to the URL
    
    Set value in URL as initial value of search keyword

--- src/app/stickers/stickers.component.ts
+++ src/app/stickers/stickers.component.ts
@@ -1,16 +1,8 @@
-import {
-  catchError,
-  Observable,
-  Subject,
-  of,
-  debounceTime,
-  distinctUntilChanged,
-  switchMap,
-} from 'rxjs';
+import {catchError, Observable, Subject, of, debounceTime, distinctUntilChanged, map} from 'rxjs';
 import {CommonModule} from '@angular/common';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {RouterLink} from '@angular/router';
+import {ActivatedRoute, Params, Router, RouterLink} from '@angular/router';
 import {
   StickerSet,
   Sticker,
@@ -21,6 +13,11 @@ import {
 } from '@modules/stickers';
 
 type UINotification = {isError: boolean; content: string};
+type RouteQueryParams = {
+  as: number; // Active Set
+  q: string; // Query
+  p: number; // Page
+};
 
 @Component({
   selector: 'app-stickers',
@@ -34,10 +31,9 @@ export class StickersComponent implements OnInit, OnDestroy {
   stickerSets$: Observable<StickerSet[]>;
   activeSetSubject$: Subject<StickerSet>;
   searchKeywordSubject$: Subject<string>;
+  doSearchingStickersSubject$: Subject<RouteQueryParams>;
 
-  activeSetId = -Infinity;
   searchKeyword = '';
-  currentPage = 1;
   pageSize = 20;
   totalPages = 1;
   visibleStickers: Sticker[] = [];
@@ -48,50 +44,59 @@ export class StickersComponent implements OnInit, OnDestroy {
   isSidebarOpen = window.innerWidth >= this.LEFT_SIDEBAR_BREAKPOINT;
 
   constructor(
+    readonly router: Router,
+    readonly activatedRoute: ActivatedRoute,
     readonly stickerSetsService: StickerSetsService,
     readonly stickersService: StickersService,
     readonly recentlyUsedStickersService: RecentlyUsedStickersService,
   ) {
     this.resizeHandler = this.resizeHandler.bind(this);
     this.searchStickers = this.searchStickers.bind(this);
+    this.fromParamsToRouteQueryParams = this.fromParamsToRouteQueryParams.bind(this);
 
     this.stickerSets$ = this.stickerSetsService.getAllStickerSets(true);
     this.activeSetSubject$ = new Subject<StickerSet>();
     this.searchKeywordSubject$ = new Subject<string>();
+    this.doSearchingStickersSubject$ = new Subject<RouteQueryParams>();
   }
 
   ngOnInit() {
     window.addEventListener('resize', this.resizeHandler);
+    this.searchKeyword = this.currentQuery.q;
+
+    this.doSearchingStickersSubject$.pipe(debounceTime(100)).subscribe(this.searchStickers);
+
+    this.activatedRoute.queryParams
+      .pipe(map(this.fromParamsToRouteQueryParams))
+      .subscribe(this.doSearchingStickersSubject$);
 
     this.stickerSets$.subscribe((stickerSets) => {
       if (Array.isArray(stickerSets) && stickerSets?.length) {
-        const activeSetId = this.activeSetId ?? 0;
+        const {as: activeSetId} = this.currentQuery;
         const nextActiveSet = stickerSets.find((s) => s.id === activeSetId) ?? stickerSets[0];
         this.activeSetSubject$.next(nextActiveSet);
       }
     });
 
-    this.activeSetSubject$
-      .pipe(
-        switchMap((activeSet) => {
-          if (this.activeSetId !== (activeSet?.id ?? 0)) {
-            this.activeSetId = activeSet?.id ?? 0;
-            this.currentPage = 1;
-          }
-          return of(activeSet);
-        }),
-      )
-      .subscribe(this.searchStickers);
+    this.activeSetSubject$.subscribe((activeSet) => {
+      const {as, ...theRestParams} = this.currentQuery;
+      if (as !== (activeSet?.id ?? 0)) {
+        this.router.navigate([], {
+          relativeTo: this.activatedRoute,
+          queryParams: {...theRestParams, as: activeSet?.id ?? 0, p: 1},
+        });
+      }
+    });
+
     this.searchKeywordSubject$
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        switchMap((searchKeyword) => {
-          this.currentPage = 1;
-          return of(searchKeyword);
-        }),
-      )
-      .subscribe(this.searchStickers);
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((searchKeyword) => {
+        const params = this.currentQuery;
+        this.router.navigate([], {
+          relativeTo: this.activatedRoute,
+          queryParams: {...params, q: searchKeyword, p: 1},
+        });
+      });
   }
 
   ngOnDestroy(): void {
@@ -101,6 +106,21 @@ export class StickersComponent implements OnInit, OnDestroy {
     }
     this.activeSetSubject$.complete();
     this.searchKeywordSubject$.complete();
+    this.doSearchingStickersSubject$.complete();
+  }
+
+  get currentQuery(): RouteQueryParams {
+    return this.fromParamsToRouteQueryParams(this.activatedRoute.snapshot.queryParams);
+  }
+
+  private fromParamsToRouteQueryParams(params: Params): RouteQueryParams {
+    const {as, q, p} = params;
+    const legitP = Math.abs(parseInt(p ?? '1') || 1);
+    return {
+      as: parseInt(as ?? '0') || 0,
+      q: q ?? '',
+      p: legitP,
+    };
   }
 
   private resizeHandler(): void {
@@ -158,12 +178,12 @@ export class StickersComponent implements OnInit, OnDestroy {
     this.searchKeywordSubject$.next(this.searchKeyword);
   }
 
-  searchStickers(): void {
-    const stickerSetId = this.activeSetId ?? 0;
-    const tagsString = this.searchKeyword ?? '';
+  searchStickers(queryParams: RouteQueryParams): void {
+    const stickerSetId = queryParams.as;
+    const tagsString = queryParams.q;
     this.stickersService
       .searchStickersBySetIdAndTags(stickerSetId, tagsString, {
-        page: this.currentPage,
+        page: queryParams.p,
         pageSize: this.pageSize,
       })
       .pipe(
@@ -172,7 +192,7 @@ export class StickersComponent implements OnInit, OnDestroy {
           return of<PaginatedResponse<Sticker>>({
             data: [],
             total: 0,
-            page: this.currentPage,
+            page: queryParams.p,
             pageSize: this.pageSize,
             totalPages: 0,
           });
@@ -185,9 +205,12 @@ export class StickersComponent implements OnInit, OnDestroy {
   }
 
   changePage(newPage: number): void {
-    if (newPage >= 1 && newPage <= this.totalPages && newPage !== this.currentPage) {
-      this.currentPage = newPage;
-      this.searchStickers();
+    const {p, ...theRestParams} = this.currentQuery;
+    if (newPage >= 1 && newPage <= this.totalPages && newPage !== p) {
+      this.router.navigate([], {
+        relativeTo: this.activatedRoute,
+        queryParams: {...theRestParams, p: newPage},
+      });
     }
   }
 }
